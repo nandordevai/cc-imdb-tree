5414d023f0a2cd07d919c527a64d6e8e596ebec2
Nandor Devai <mail@nandordevai.net>
2019-11-02 21:57:49 +0100

    add text & ordering

--- chart.js
+++ chart.js
@@ -1,16 +1,25 @@
 d3.json('./top-1000-extended-posters.json')
-    .then(function(data) {
-        const slice = data.filter(function(_) { return _.year >= 2017 && _.metascore !== ''; });
+    .then(function (data) {
+        const slice = data.filter(function (_) { return _.year >= 2017 && _.metascore !== ''; });
         const movies = d3.nest()
-            .key(function(d) { return d.year; }).sortKeys(d3.descending)
-            .key(function(d) { return d.genre.split(', ')[0]; }).sortKeys(d3.ascending)
+            .key(function (d) { return d.year; }).sortKeys(d3.descending)
+            .key(function (d) { return d.genre.split(', ')[0]; }).sortKeys(d3.ascending)
+            .sortValues((a, b) => {
+                if (a.title_eng > b.title_eng) {
+                    return 1;
+                } else if (a.title_eng < b.title_eng) {
+                    return -1
+                } else {
+                    return 0;
+                }
+            })
             .entries(slice);
         const root = d3.hierarchy(
             {
                 title_eng: 'IMDB Top',
                 values: movies,
             },
-            function(d) { return d.values; });
+            function (d) { return d.values; });
         const treeLayout = d3.tree();
         treeLayout.size([1500, 800]);
         treeLayout(root);
@@ -23,24 +32,30 @@ function draw(root) {
         .selectAll('g')
         .data(root.descendants())
         .join(
-            function(enter) { return enter.append('g'); }
+            function (enter) { return enter.append('g'); }
         );
 
     movies.append('circle')
-        .attr('cx', function(d) { return d.y; })
-        .attr('cy', function(d) { return d.x; })
+        .attr('cx', function (d) { return d.y; })
+        .attr('cy', function (d) { return d.x; })
         .attr('r', 10);
+
+    movies.append('text')
+        .text(function (d) { return d.data.title_eng || d.data.key; })
+        .attr('x', function (d) { return d.data.title_eng ? d.y + 30 : d.y - 20; })
+        .attr('y', function (d) { return d.x; })
+        .classed('title', function (d) { return d.data.title_eng; })
 
     // Links
     const link = d3.linkHorizontal()
-        .x(function(d) { return d.y; })
-        .y(function(d) { return d.x; });
+        .x(function (d) { return d.y; })
+        .y(function (d) { return d.x; });
 
     d3.select('svg g.links')
         .selectAll('path.link')
         .data(root.links())
         .join(
-            function(enter) {
+            function (enter) {
                 return enter.append('path')
                     .classed('link', true)
                     .attr('d', link)
